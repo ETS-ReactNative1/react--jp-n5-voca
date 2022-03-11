bb658016f9ab9b7d7d8ba36891c3efdebf224b67
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-03-11 22:14:25 +0630

    added loading for fallback state

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -38,12 +38,12 @@ const NavBar = ({ currentLang: lang, langChange }) => {
           onClick={() => setMenuVisibility(!menuVisibility)}
           data-collapse-toggle="mobile-menu"
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="mobile-menu-2"
           aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <svg
-            className="w-6 h-6"
+            className={`w-6 h-6 ${menuVisibility ? 'hidden' : ''}`}
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg">
@@ -53,6 +53,19 @@ const NavBar = ({ currentLang: lang, langChange }) => {
               clipRule="evenodd"></path>
           </svg>
           <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`w-6 h-6 ${menuVisibility ? '' : 'hidden'}`}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+          {/* <svg
             className="hidden w-6 h-6"
             fill="currentColor"
             viewBox="0 0 20 20"
@@ -61,7 +74,7 @@ const NavBar = ({ currentLang: lang, langChange }) => {
               fillRule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
               clipRule="evenodd"></path>
-          </svg>
+          </svg> */}
         </button>
         <div
           className={`w-full md:block md:w-auto   ${
